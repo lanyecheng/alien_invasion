1b6d237e7e83633e69570c9dc3312cade955e080
LanYc <lanyecheng@126.com>
2019-12-21 14:05:36 +0800

    飞船左右移动控制

--- settings.py
+++ settings.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8 -*-
 # author：albert time:2019/12/21
 
+
 class Settings:
+    """存储游戏所有设置的类"""
 
     def __init__(self):
-
+        """初始化游戏的设置"""
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        # 飞船的设置
+        self.ship_speed_factor = 1.5
