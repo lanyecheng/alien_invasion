af38bbf0dffd62d295547bc29b8c1ab715061eec
LanYc <lanyecheng@126.com>
2019-12-21 17:06:05 +0800

    添加外星人 -- p241

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,11 +17,10 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
 
-    # 创建一艘飞船
+    # 创建一艘飞船、一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
-
-    # 创建一个用于存储子弹的编组
     bullets = Group()
+    aliens = Group()
 
     # 开始游戏的主循环
     while True:
@@ -31,8 +30,9 @@ def run_game():
         ship.update()
         # 更新子弹位置
         gf.update_bullets(bullets)
+        gf.create_fleet(ai_settings, screen, ship, aliens)
         # 使用更新后的位置绘制屏幕
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
