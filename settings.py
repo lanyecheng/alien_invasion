a7de267b3514545b89b974502838dcc46deebcdc
LanYc <lanyecheng@126.com>
2019-12-21 12:25:25 +0800

    创建settings模块，以及游戏所需的图片 -- p210

---
+++ settings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author：albert time:2019/12/21
+
+class Settings():
+
+    def __init__(self):
+
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
