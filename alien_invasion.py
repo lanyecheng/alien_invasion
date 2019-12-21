ecd677f93fc0bbf05dca96cc8ddbb1e48c5e0c09
LanYc <lanyecheng@126.com>
2019-12-21 20:30:18 +0800

    移动 -- p245

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,6 +22,9 @@ def run_game():
     bullets = Group()
     aliens = Group()
 
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
     # 开始游戏的主循环
     while True:
         # 检查键盘
@@ -30,7 +33,7 @@ def run_game():
         ship.update()
         # 更新子弹位置
         gf.update_bullets(bullets)
-        gf.create_fleet(ai_settings, screen, ship, aliens)
+        gf.update_aliens(ai_settings, aliens)
         # 使用更新后的位置绘制屏幕
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
--- settings.py
+++ settings.py
@@ -21,3 +21,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullet_allowed = 5
+
+        # 外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction 为1表示向右移动，为-1表示向左移动
+        self.fleet_direction = 1
